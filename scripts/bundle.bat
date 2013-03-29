4b8979cdd5eb2eea0b1e9a0d0ff53aee29990f04
Kevin Fox <Kevin.Fox@pnl.gov>
2013-03-29 16:44:36 -0700

    Work around wix bug for now.

--- scripts/bundle.bat
+++ scripts/bundle.bat
@@ -1,5 +1,6 @@
 set PATH=%PATH%;c:\wix
-heat dir "build\release" -srd -cg PacificaAuthRelease -gg -sfrag -dr INSTALLDIR -var var.PacificaAuthRelease -out pacificaauthrelease.wxs || goto :error
+heat dir "build\release" -srd -cg PacificaAuthRelease -gg -sfrag -dr INSTALLDIR -var var.PacificaAuthRelease -out pacificaauthrelease.wxs
+rem Comment this out while wix heat is broken. || goto :error
 candle pacificaauth.wxs pacificaauthrelease.wxs -dPacificaAuthRelease=build\release || goto :error
 light -out pacificaauth pacificaauth.wixobj pacificaauthrelease.wixobj || goto :error
 heat dir "go\pkg" -cg PacificaAuthGO -gg -sfrag -dr GODIR -var var.PacificaGO -out pacificaauthgo.wxs || goto :error
